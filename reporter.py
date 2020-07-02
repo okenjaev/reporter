833dc7ae1ddb0e652aa993aee38ef4fc34ca28aa
Olimjon Kenjaev <ocuredev@gmail.com>
2020-07-03 02:59:45 +0500

    release 0.0.1

--- reporter.py
+++ reporter.py
@@ -2,28 +2,42 @@
 
 import subprocess
 import os
-from collections import OrderedDict
 from datetime import datetime
 
-p = subprocess.Popen(["git", "log", "--pretty=format:'%at %s'"], cwd=os.getcwd(), stdout = subprocess.PIPE)
+def systemCall(args):
+    p = subprocess.Popen(args, cwd = os.getcwd(), stdout = subprocess.PIPE, universal_newlines=True)
+    out, err = p.communicate()
+    if err is not None:
+        print(err)
+        quit()
+    else:
+        return out
 
-out, err = p.communicate()
+author = systemCall(["git", "config", "user.name"]).replace('\n', '').replace('\'', '')
+print("Generating report for user " + author)
 
-if err is not None:    
-    print(err)
-    quit()
+today = datetime.today()
 
-real_out=out.replace("'", "").split("\n")
+args = ["git",
+        "--no-pager",
+        "log",
+        "--since=\"{0}-{1}-01\"".format(today.year, today.month),
+        "--no-merges",
+        "--pretty=format:%an, %at, %s"]
 
+real_out = systemCall(args).split('\n')
 last_date = datetime.fromtimestamp(0)
-for it in real_out:
-    time, message = it.split(' ', 1)
 
+for it in real_out:
+    committer, time, message = it.split(', ',2)
+    if committer != author:
+        continue
+    
     date = datetime.fromtimestamp(float(time))
     
-    if last_date.day == date.day:
-        print("\t" + message)
+    if last_date.date() == date.date():
+        print("\t " + message)
     else:
         print(date.strftime("%d.%m.%Y:"))
-        print("\t" + message)
+        print("\t " + message)
         last_date = date
